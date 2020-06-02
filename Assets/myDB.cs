fdb98296a3a478556a387b16a51b3fa6d70372aa
Vered Sayag <vered8765@gmail.com>
2020-06-02 14:54:19 +0300

    setings

--- Assets/myDB.cs
+++ Assets/myDB.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
  using System;
  using System.Data;
+using System.Collections.Generic;
 using MySql.Data;
  using MySql.Data.MySqlClient;
 using System.Linq;
