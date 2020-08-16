df358f68d42ad06fed3bdd2e9865a9bf6f134b6d
Ariel Yaaran <ariel.yaaran@live.biu.ac.il>
2020-08-16 12:56:49 +0300

    placeholder game over
    does not save ANY information about the game, needs fixing. still better than an infinite game.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,15 +7,32 @@ using UnityEngine.SceneManagement;
 public static class GameManager
 {
     private const float INIT_WORD_DELAY = 1.7f;
+    private static int heartAmount = 4;
 
     public static int LevelNum { get; set; } = 1;
     public static float FallSpeed { get; set; } = 2f;
     public static float WordDelay { get; set; } = INIT_WORD_DELAY;
     public static float CupSpeed { get; set; } = 10;
-    public static int HeartAmount { get; set; } = 4;
+    public static int HeartAmount {
+        get => heartAmount;
+        set {
+            if (value <= 0)
+                gameOver();
+            else
+                heartAmount = value;
+        }
+    }
 
-
-
+    public static void gameOver()
+    {
+        WordDelay = INIT_WORD_DELAY;
+        FallSpeed = 2f;
+        CupSpeed = 10f;
+        HeartAmount = 4;
+        LevelNum = 1;
+        score.scoreVal = 0;
+        SceneManager.LoadScene("menuScene");
+    }
 
     public static void nextLevel()
     {
